f3184ca4de1842c1e4a754fd01573ab5a3f72d32
Austin Conner <austin.d.conner@vanderbilt.edu>
2011-01-14 10:02:45 -0600

    Added tunable file

--- state/recycler/RecyclerAbstractState.java
+++ state/recycler/RecyclerAbstractState.java
@@ -4,14 +4,11 @@ import battlecode.common.Clock;
 import battlecode.common.RobotController;
 import hex.data.RobotControls;
 import hex.data.Schematics;
+import hex.data.Tunable;
 import hex.state.AbstractState;
 
 // this class exists to bring the recycler state change information to one place
 public abstract class RecyclerAbstractState extends AbstractState {
-	
-	public static final int ROUNDS_TO_PRODUCE_BUILDERS = 200;
-	public static final int ROUNDS_TO_STOP_LIGHT_PRODUCTION = 2000;
-	public static final int MINIMUM_FLUX_LIGHT_PRODUCTION = 30;
 
 	public RecyclerAbstractState(RobotController RC, RobotControls comp) {
 		super(RC, comp);
@@ -23,9 +20,9 @@ public abstract class RecyclerAbstractState extends AbstractState {
     	myRC.setIndicatorString(0, String.valueOf(myRC.getTeamResources()));
     	
 		// TODO modify when recyclers should be in production mode
-		if (Clock.getRoundNum() < ROUNDS_TO_PRODUCE_BUILDERS)
+		if (Clock.getRoundNum() < Tunable.RECYCLER_ROUNDS_TO_PRODUCE_BUILDERS)
 			return new RecyclerBuildSchematic(myRC, robotComps, Schematics.lightConstructor);
-		else if (myRC.getTeamResources() >= MINIMUM_FLUX_LIGHT_PRODUCTION  && Clock.getRoundNum() < ROUNDS_TO_STOP_LIGHT_PRODUCTION)
+		else if (myRC.getTeamResources() >= Tunable.RECYCLER_MINIMUM_FLUX_LIGHT_PRODUCTION  && Clock.getRoundNum() < Tunable.RECYCLER_ROUNDS_TO_STOP_LIGHT_PRODUCTION)
 			return new RecyclerBuildSchematic(myRC, robotComps, Schematics.lightAttack);
 		else return new RecyclerIdleState(myRC, robotComps);
 	}
